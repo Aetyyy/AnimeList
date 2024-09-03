e0159782e9344e3671ce9abb78ebe1aa19e4ced4
Aetyyy <aethy.games@gmail.com>
2024-09-04 01:16:47 +0700

    Updating Margins

--- src/app/search/[keywords]/page.tsx
+++ src/app/search/[keywords]/page.tsx
@@ -1,16 +1,20 @@
-import AnimeList from "@/components/Home"
+import AnimeList from "@/components/Home";
 import { AnimeData } from "@/interfaces/animedata.i";
 
-export default async function Page({ params }: { params: { keywords: string } }) {
-  const { keywords } = params
+export default async function Page({
+  params,
+}: {
+  params: { keywords: string };
+}) {
+  const { keywords } = params;
   const res = await fetch(`${process.env.API_ANIME}/anime?q=${keywords}`);
   const searchAnime: AnimeData = await res.json();
 
   return (
     <>
-    <section>
-      <AnimeList api={searchAnime} />
-    </section>
+      <section className="mt-[4rem]">
+        <AnimeList api={searchAnime} />
+      </section>
     </>
-  )
+  );
 }
--- src/components/Home/Information.tsx
+++ src/components/Home/Information.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Headers({ title, description }: { title: string; description: string }) {
   return (
-    <div className="container mx-auto lg:max-w-[60rem] p-4 bg-[#222222] mt-8 pb-8">
+    <div className="container mx-auto lg:max-w-[60rem] p-4 bg-[#222222] mt-36 pb-8">
       <div className="flex flex-col items-center text-[#d4d4d4]">
         <h1 className="text-2xl font-bold mb-4">{title}</h1>
         <p className="px-6 text-pretty">{description}</p>
