f1e763eb31ae49a9c1576f4c013b9a7f8276caf6
Aetyyy <aethy.games@gmail.com>
2024-09-04 01:18:44 +0700

    Updating Image size at card

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -20,43 +20,42 @@ type TopAnimeProps = {
 };
 
 export default function TopAnime({ api }: TopAnimeProps) {
-
   return (
     <div className="container mx-auto lg:max-w-[60rem] mb-16">
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-2  pb-16 md:px-20 bg-[#222222]">
         {api.data.map((i) => (
           <div key={i.mal_id} className="flex justify-center mt-6">
-        <Link
-          href={`/${i.mal_id}`}
-          className="cursor-pointer hover:scale-[105%] transition-transform duration-300"
-        >
-          <div className="w-[180px] flex flex-col">
-            <Image
-              className="w-full max-h-[110px] object-cover rounded-sm"
-              src={i.images.webp.large_image_url}
-              alt={i.title + "Banners"}
-              width={250}
-              height={110}
-              priority={true}
-            />
-            <div className="pt-4 flex-grow flex flex-col justify-between sm:h-[8rem] md:h-[9rem]">
-              <h5 className="mb-1 md:text-2xl font-semibold line-clamp-1 sm:line-clamp-2 text-[#d4d4d4] hover:text-white">
-                {i.title}
-              </h5>
-              <p className="mb-1 font-normal text-[#737373] line-clamp-3">
-                <span className="flex items-center">
-                  <RiUserFill className="mr-2" />
-                  Uploader&nbsp;:&nbsp;Aetyyy
-                </span>
-                <span className="flex items-center">
-                  <BsFillPlayFill className="mr-2" />
-                  Episode&nbsp;&nbsp;&nbsp;:&nbsp;{i.episodes}
-                </span>
-              </p>
-            </div>
+            <Link
+              href={`/${i.mal_id}`}
+              className="cursor-pointer hover:scale-[105%] transition-transform duration-300"
+            >
+              <div className="w-[180px] flex flex-col">
+                <Image
+                  className="max-w-[180px] max-h-[95px] object-cover rounded-sm"
+                  src={i.images.webp.large_image_url}
+                  alt={i.title + "Banners"}
+                  width={350}
+                  height={350}
+                  priority={true}
+                />
+                <div className="pt-4 flex-grow flex flex-col justify-between sm:h-[8rem] md:h-[9rem]">
+                  <h5 className="mb-1 md:text-2xl font-semibold line-clamp-1 sm:line-clamp-2 text-[#d4d4d4] hover:text-white">
+                    {i.title}
+                  </h5>
+                  <p className="mb-1 font-normal text-[#737373] line-clamp-3">
+                    <span className="flex items-center">
+                      <RiUserFill className="mr-2" />
+                      Uploader&nbsp;:&nbsp;Aetyyy
+                    </span>
+                    <span className="flex items-center">
+                      <BsFillPlayFill className="mr-2" />
+                      Episode&nbsp;&nbsp;&nbsp;:&nbsp;{i.episodes}
+                    </span>
+                  </p>
+                </div>
+              </div>
+            </Link>
           </div>
-        </Link>
-        </div>
         ))}
       </div>
     </div>
